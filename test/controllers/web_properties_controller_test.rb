cd680d41bdff728eff3bb9cad07ae90bbd98d199
John Hutchinson <johnhutch@gmail.com>
2022-07-07 17:16:25 -0400

    Cleaned up web properties controller and rails test.

--- test/controllers/web_properties_controller_test.rb
+++ test/controllers/web_properties_controller_test.rb
@@ -2,47 +2,48 @@ require "test_helper"
 
 class WebPropertiesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @client = clients(:one)
     @web_property = web_properties(:one)
   end
 
   test "should get index" do
-    get web_properties_url
+    get client_web_properties_url(@client)
     assert_response :success
   end
 
   test "should get new" do
-    get new_web_property_url
+    get new_client_web_property_url(@client)
     assert_response :success
   end
 
   test "should create web_property" do
     assert_difference("WebProperty.count") do
-      post web_properties_url, params: { web_property: { client_id: @web_property.client_id, name: @web_property.name, url: @web_property.url } }
+      post client_web_properties_url(@client), params: { web_property: { client_id: @web_property.client_id, name: @web_property.name, url: @web_property.url } }
     end
 
-    assert_redirected_to web_property_url(WebProperty.last)
+    assert_redirected_to client_url(@client)
   end
 
   test "should show web_property" do
-    get web_property_url(@web_property)
+    get client_web_property_url(@client, @web_property)
     assert_response :success
   end
 
   test "should get edit" do
-    get edit_web_property_url(@web_property)
+    get edit_client_web_property_url(@client, @web_property)
     assert_response :success
   end
 
   test "should update web_property" do
-    patch web_property_url(@web_property), params: { web_property: { client_id: @web_property.client_id, name: @web_property.name, url: @web_property.url } }
-    assert_redirected_to web_property_url(@web_property)
+    patch client_web_property_url(@client, @web_property), params: { web_property: { client_id: @web_property.client_id, name: @web_property.name, url: @web_property.url } }
+    assert_redirected_to client_url(@client)
   end
 
   test "should destroy web_property" do
     assert_difference("WebProperty.count", -1) do
-      delete web_property_url(@web_property)
+      delete client_web_property_url(@client, @web_property)
     end
 
-    assert_redirected_to web_properties_url
+    assert_redirected_to client_url(@client)
   end
 end
